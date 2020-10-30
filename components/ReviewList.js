7e06ac9d36331ef96b97fd8be471746a4d5942b5
p2well <pawel@p2well.com>
2020-10-30 13:11:34 +0100

    Accomplish lesson 11 coding challenge

--- components/ReviewList.js
+++ components/ReviewList.js
@@ -15,6 +15,8 @@ app.component('review-list', {
         {{ review.name }} gave this {{ review.rating }} stars
         <br/>
         "{{ review.review }}"
+        <br/>
+        Recommended: {{ review.recommend }}
       </li>
     </ul>
   </div>
